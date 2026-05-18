1acd92b9bc4dd3084c1ce71ce63e49cbff931b90
Kirilll.212 <kiril.kushnir@rexsoftinc.co>
2026-05-18 15:51:07 +0300

    add pm2 ecosystem

---
+++ ecosystem.config.cjs
@@ -0,0 +1,23 @@
+module.exports = {
+  apps: [
+    {
+      name: 'Ink Muse API',
+      script: 'node ./build/bin/server.js',
+      error_file: './tmp/err.log',
+      out_file: './tmp/out.log',
+      log_file: './tmp/combined.log',
+      log_date_format: 'YYYY-MM-DD_HH-mm-ss',
+      ignore_watch: [
+        'node_modules',
+        'public',
+        'tmp',
+        'storage',
+        '.git',
+        '.history',
+        '.idea',
+        '.vscode',
+        '.cache',
+      ],
+    },
+  ],
+}
